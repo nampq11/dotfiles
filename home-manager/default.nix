a76ac52ff08c6ba5d4ef089d81d822e598d99e5d
nam pham <nampham11062002@gmail.com>
2026-01-20 16:22:07 +0700

    chore: creat home-manager

---
+++ home-manager/default.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  username,
+  ...
+}:
+let
+  hmConfig = import ../config;
+  packages = import ./packages { inherit pkgs inputs; };
+  misc = import ./misc;
+  modules = import ./modules;
+  programs = import ./programs {
+    inherit config lib pkgs;
+    sources = { };
+  };
+  services = import ./services {
+    inherit config lib pkgs;
+  };
+in
+{
+  imports =
+    hmConfig
+    ++ misc
+    ++ modules
+    ++ programs
+    ++ services
+    ++ [
+      inputs.agenix.homeManagerModules.default
+      inputs.nix-clawdbot.homeManagerModules.clawdbot
+    ];
+
+  home.username = username;
+  home.homeDirectory = lib.mkIf pkgs.stdenv.isLinux "/home/${username}";
+  home.packages = packages;
+  home.stateVersion = "24.11";
+
+  accounts.email.accounts = {
+    Gmail = {
+      primary = true;
+      flavor = "gmail.com";
+      realName = "Shun Kakinoki";
+      address = "shunkakinoki@gmail.com";
+    };
+  };
+}
